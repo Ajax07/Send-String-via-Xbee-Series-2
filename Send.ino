c4a62b3942a1a723cb90b63d708f2f040da08947
Ajay Singh Chouhan <38866224+Ajax07@users.noreply.github.com>
2018-07-11 19:11:51 +0530

    Add files via upload

---
+++ Send.ino
@@ -0,0 +1,35 @@
+void Send(uint8_t addr){
+ // if Data is not Transmitted then manually put you Xbee address here
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, addr);
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+//  XBSetting();
+  xbee.send(zbTx);
+  if (xbee.readPacket(500)) {
+    // got a response!
+
+    // should be a znet tx status              
+    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+      xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+      
+      if (txStatus.getDeliveryStatus() == SUCCESS) {
+        // success.  time to celebrate
+        Serial.println("data send");
+
+      } else {
+        
+        Serial.println("data not send");
+
+      }
+    }
+  } else if (xbee.getResponse().isError()) {
+    //nss.print("Error reading packet.  Error code: ");  
+    //nss.println(xbee.getResponse().getErrorCode());
+  } else {
+    // local XBee did not provide a timely TX Status Response -- should not happen
+
+  }
+
+  delay(1000);
+}
